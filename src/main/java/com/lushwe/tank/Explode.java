ae33ce06ded52768415b8af6fa978fc50f26b5da
Jack Liu <947613622@qq.com>
2020-11-21 15:08:43 +0800

    爆炸结束从列表中删除

--- src/main/java/com/lushwe/tank/Explode.java
+++ src/main/java/com/lushwe/tank/Explode.java
@@ -35,7 +35,7 @@ public class Explode {
         g.drawImage(ResourceMgr.explodes[step++], x, y, null);
 
         if (step >= ResourceMgr.explodes.length) {
-            step = 0;
+            this.tf.explodes.remove(this);
         }
     }
 }
