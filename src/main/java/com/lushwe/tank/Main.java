27f574a6f026313941048770a952ee6abdd824f3
Jack Liu <947613622@qq.com>
2020-12-07 22:09:28 +0800

    注释敌人坦克初始化，为网络版本准备

--- src/main/java/com/lushwe/tank/Main.java
+++ src/main/java/com/lushwe/tank/Main.java
@@ -13,12 +13,12 @@ public class Main {
 
         TankFrame tf = new TankFrame();
 
-        int initTankCount = PropertyMgr.getInt("initTankCount");
-
-        // 初始化敌方坦克
-        for (int i = 0; i < initTankCount; i++) {
-            tf.tankList.add(new Tank(100 + 100 * i, 200, Dir.DOWN, Group.BAD, tf));
-        }
+//        int initTankCount = PropertyMgr.getInt("initTankCount");
+//
+//        // 初始化敌方坦克
+//        for (int i = 0; i < initTankCount; i++) {
+//            tf.tankList.add(new Tank(100 + 100 * i, 200, Dir.DOWN, Group.BAD, tf));
+//        }
 
         new Thread(() -> new Audio("audio/war1.wav").loop()).start();
 
