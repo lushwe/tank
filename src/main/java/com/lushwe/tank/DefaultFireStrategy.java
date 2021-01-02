6fc610ccbb0d2f2de5dbfea52b1cc8eaafd0a915
Jack Liu <947613622@qq.com>
2021-01-02 23:17:28 +0800

    默认开火策略

---
+++ src/main/java/com/lushwe/tank/DefaultFireStrategy.java
@@ -0,0 +1,24 @@
+package com.lushwe.tank;
+
+/**
+ * 说明：默认开火策略
+ *
+ * @author Jack Liu
+ * @date 2021-01-02 22:54
+ * @since 0.1
+ */
+public class DefaultFireStrategy implements FireStrategy {
+
+    @Override
+    public void fire(Tank tank) {
+
+        int x = tank.getX() + Tank.WIDTH / 2 - Bullet.WIDTH / 2;
+        int y = tank.getY() + Tank.HEIGHT / 2 - Bullet.HEIGHT / 2;
+
+        new Bullet(x, y, tank.getDir(), tank.getGroup(), tank.getGm());
+
+        if (tank.getGroup() == Group.GOOD) {
+            new Thread(() -> new Audio("audio/tank_fire.wav").play()).start();
+        }
+    }
+}
