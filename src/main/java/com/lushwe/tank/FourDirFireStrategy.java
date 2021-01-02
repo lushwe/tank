809a0f27d46378f8dba47ef59108228a1920a2e1
Jack Liu <947613622@qq.com>
2021-01-02 23:17:46 +0800

    四个方向开火策略

---
+++ src/main/java/com/lushwe/tank/FourDirFireStrategy.java
@@ -0,0 +1,27 @@
+package com.lushwe.tank;
+
+/**
+ * 说明：四个方向开火策略
+ *
+ * @author Jack Liu
+ * @date 2021-01-02 22:54
+ * @since 0.1
+ */
+public class FourDirFireStrategy implements FireStrategy {
+
+    @Override
+    public void fire(Tank tank) {
+
+        int x = tank.getX() + Tank.WIDTH / 2 - Bullet.WIDTH / 2;
+        int y = tank.getY() + Tank.HEIGHT / 2 - Bullet.HEIGHT / 2;
+
+
+        for (Dir dir : Dir.values()) {
+            new Bullet(x, y, dir, tank.getGroup(), tank.getGm());
+        }
+
+        if (tank.getGroup() == Group.GOOD) {
+            new Thread(() -> new Audio("audio/tank_fire.wav").play()).start();
+        }
+    }
+}
