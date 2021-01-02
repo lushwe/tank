d2dfb8f3372b28d05717328719798daca521ac69
Jack Liu <947613622@qq.com>
2021-01-02 23:17:05 +0800

    抽象开火策略

---
+++ src/main/java/com/lushwe/tank/FireStrategy.java
@@ -0,0 +1,18 @@
+package com.lushwe.tank;
+
+/**
+ * 说明：开火策略
+ *
+ * @author Jack Liu
+ * @date 2021-01-02 22:53
+ * @since 0.1
+ */
+public interface FireStrategy {
+
+    /**
+     * 开火
+     *
+     * @param tank
+     */
+    void fire(Tank tank);
+}
