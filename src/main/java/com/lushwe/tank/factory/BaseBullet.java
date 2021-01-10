49347b7c398ab752067138c5615da90263dca1af
Jack Liu <947613622@qq.com>
2021-01-10 20:52:22 +0800

    基础子弹对象

---
+++ src/main/java/com/lushwe/tank/factory/BaseBullet.java
@@ -0,0 +1,20 @@
+package com.lushwe.tank.factory;
+
+import java.awt.*;
+
+/**
+ * 说明：基础子弹对象
+ *
+ * @author Jack Liu
+ * @date 2021/1/10 下午8:40
+ * @since 0.1
+ */
+public abstract class BaseBullet {
+
+    /**
+     * 画自己
+     *
+     * @param g
+     */
+    public abstract void paint(Graphics g);
+}
