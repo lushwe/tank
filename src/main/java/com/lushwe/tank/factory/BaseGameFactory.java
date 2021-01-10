e8de70baecae66f8a7cd0a685f57addfcd8b9c63
Jack Liu <947613622@qq.com>
2021-01-10 20:53:59 +0800

    基础游戏工程对象

---
+++ src/main/java/com/lushwe/tank/factory/BaseGameFactory.java
@@ -0,0 +1,49 @@
+package com.lushwe.tank.factory;
+
+import com.lushwe.tank.GameModel;
+import com.lushwe.tank.enums.Dir;
+import com.lushwe.tank.enums.Group;
+
+/**
+ * 说明：基础游戏工厂
+ *
+ * @author Jack Liu
+ * @date 2021-1-10 20:27
+ * @since 0.1
+ */
+public abstract class BaseGameFactory {
+
+    /**
+     * 创建坦克
+     *
+     * @param x
+     * @param y
+     * @param dir
+     * @param group
+     * @param gm
+     * @return
+     */
+    public abstract BaseTank createTank(int x, int y, Dir dir, Group group, GameModel gm);
+
+    /**
+     * 创建子弹
+     *
+     * @param x
+     * @param y
+     * @param dir
+     * @param group
+     * @param gm
+     * @return
+     */
+    public abstract BaseBullet createBullet(int x, int y, Dir dir, Group group, GameModel gm);
+
+    /**
+     * 创建爆炸
+     *
+     * @param x
+     * @param y
+     * @param gm
+     * @return
+     */
+    public abstract BaseExplode createExplode(int x, int y, GameModel gm);
+}
