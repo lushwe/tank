7aa3ccc9db6228c59bc8e9866135081ddc3b28a0
Jack Liu <947613622@qq.com>
2021-01-10 20:52:32 +0800

    基础爆炸对象

---
+++ src/main/java/com/lushwe/tank/factory/BaseExplode.java
@@ -0,0 +1,20 @@
+package com.lushwe.tank.factory;
+
+import java.awt.*;
+
+/**
+ * 说明：基础爆炸对象
+ *
+ * @author Jack Liu
+ * @date 2021/1/10 下午8:41
+ * @since 0.1
+ */
+public abstract class BaseExplode {
+
+    /**
+     * 画自己
+     *
+     * @param g
+     */
+    public abstract void paint(Graphics g);
+}
