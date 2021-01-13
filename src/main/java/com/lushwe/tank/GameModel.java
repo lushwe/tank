3e0e60aea7405da94bee52536978bd52274d7c82
Jack Liu <947613622@qq.com>
2021-01-13 21:10:12 +0800

    游戏模型对象调整

--- src/main/java/com/lushwe/tank/GameModel.java
+++ src/main/java/com/lushwe/tank/GameModel.java
@@ -3,8 +3,6 @@ package com.lushwe.tank;
 import com.lushwe.tank.enums.Dir;
 import com.lushwe.tank.enums.Group;
 import com.lushwe.tank.factory.GameFactory;
-import com.lushwe.tank.model.Bullet;
-import com.lushwe.tank.model.Explode;
 import com.lushwe.tank.model.Tank;
 import com.lushwe.tank.util.PropertyUtils;
 
@@ -32,26 +30,17 @@ public class GameModel {
     private List<GameObject> gameObjects = new ArrayList<>();
 
     /**
-     * 敌人坦克
-     */
-    private List<Tank> tankList = new ArrayList<>();
-
-    /**
-     * 子弹
-     */
-    private List<Bullet> bulletList = new ArrayList<>();
-
-    /**
-     * 爆炸
-     */
-    private List<Explode> explodes = new ArrayList<>();
-
-    /**
      * 游戏工厂
      */
     private GameFactory gameFactory;
 
+    private List<Collider> colliders = new ArrayList<>();
+
     public GameModel() {
+
+        //
+//        colliders.add(new BulletTankCollider());
+//        colliders.add(new TankTankCollider());
 
         // 初始化游戏工厂
         String gameFactoryName = PropertyUtils.getString("gameFactory");
@@ -67,14 +56,24 @@ public class GameModel {
         // 初始化敌方坦克
         int initTankCount = PropertyUtils.getInt("initTankCount");
         for (int i = 0; i < initTankCount; i++) {
-            this.tankList.add(this.gameFactory.createTank(100 + 100 * i, 200, Dir.DOWN, Group.BAD, this));
+            this.gameObjects.add(this.gameFactory.createTank(100 + 100 * i, 200, Dir.DOWN, Group.BAD, this));
         }
     }
 
+    /**
+     * 添加对象
+     *
+     * @param go
+     */
     public void add(GameObject go) {
         this.gameObjects.add(go);
     }
 
+    /**
+     * 移除对象
+     *
+     * @param go
+     */
     public void remove(GameObject go) {
         this.gameObjects.remove(go);
     }
@@ -86,33 +85,25 @@ public class GameModel {
         // 画出子弹数量
         Color color = g.getColor();
         g.setColor(Color.WHITE);
-        g.drawString("子弹数量：" + bulletList.size(), 10, 50);
-        g.drawString("敌人数量：" + tankList.size(), 10, 70);
-        g.drawString("爆炸数量：" + explodes.size(), 10, 90);
+//        g.drawString("子弹数量：" + bulletList.size(), 10, 50);
+//        g.drawString("敌人数量：" + tankList.size(), 10, 70);
+//        g.drawString("爆炸数量：" + explodes.size(), 10, 90);
         g.setColor(color);
 
-        // 画坦克
+        // 画主坦克
         mainTank.paint(g);
 
-        // 画坦克
-        for (int i = 0; i < tankList.size(); i++) {
-            tankList.get(i).paint(g);
-        }
-
-        // 画子弹
-        for (int i = 0; i < bulletList.size(); i++) {
-            bulletList.get(i).paint(g);
-        }
-
-        // 画爆炸
-        for (int i = 0; i < explodes.size(); i++) {
-            explodes.get(i).paint(g);
+        // 画游戏对象（坦克、子弹、爆炸）
+        for (int i = 0; i < gameObjects.size(); i++) {
+            gameObjects.get(i).paint(g);
         }
 
         // 碰撞检测
-        for (int i = 0; i < bulletList.size(); i++) {
-            for (int j = 0; j < tankList.size(); j++) {
-                bulletList.get(i).collideWith(tankList.get(j));
+        for (int i = 0; i < gameObjects.size(); i++) {
+            for (int j = i + 1; j < gameObjects.size(); j++) {
+                for (Collider collider : colliders) {
+                    collider.collide(gameObjects.get(i), gameObjects.get(j));
+                }
             }
         }
     }
@@ -124,18 +115,6 @@ public class GameModel {
      */
     public Tank getMainTank() {
         return mainTank;
-    }
-
-    public List<Tank> getTankList() {
-        return tankList;
-    }
-
-    public List<Bullet> getBulletList() {
-        return bulletList;
-    }
-
-    public List<Explode> getExplodes() {
-        return explodes;
     }
 
     public GameFactory getGameFactory() {
