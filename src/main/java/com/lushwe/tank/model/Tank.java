6a3ede63f2417690d975bce9c1bd8b0cca0c41ad
Jack Liu <947613622@qq.com>
2021-01-16 22:39:14 +0800

    坦克基类调整

--- src/main/java/com/lushwe/tank/model/Tank.java
+++ src/main/java/com/lushwe/tank/model/Tank.java
@@ -2,8 +2,10 @@ package com.lushwe.tank.model;
 
 import com.lushwe.tank.GameModel;
 import com.lushwe.tank.GameObject;
+import com.lushwe.tank.TankFrame;
 import com.lushwe.tank.enums.Dir;
 import com.lushwe.tank.enums.Group;
+import com.lushwe.tank.model.def.DefaultTank;
 import com.lushwe.tank.util.ResourceUtils;
 
 import java.awt.*;
@@ -20,6 +22,9 @@ public abstract class Tank extends GameObject {
     public static int WIDTH = ResourceUtils.goodTankU.getWidth();
     public static int HEIGHT = ResourceUtils.goodTankU.getHeight();
 
+    protected int oldX;
+    protected int oldY;
+
     public abstract int getX();
 
     public abstract int getY();
@@ -28,18 +33,38 @@ public abstract class Tank extends GameObject {
 
     public abstract void setDir(Dir dir);
 
-    public abstract Dir getReverseDir();
-
     public abstract void setMoving(boolean moving);
 
     public abstract Group getGroup();
 
     public abstract GameModel getGm();
 
+    public abstract Rectangle getRect();
+
     public abstract void die();
 
     public abstract void fire();
 
-    public abstract Rectangle getRect();
+    public void back() {
+        this.x = this.oldX;
+        this.y = this.oldY;
+    }
 
+    /**
+     * 边界检查
+     */
+    protected void boundsCheck() {
+        if (this.x < 2) {
+            x = 2;
+        }
+        if (this.y < 28) {
+            y = 28;
+        }
+        if (this.x > TankFrame.GAME_WIDTH - DefaultTank.WIDTH - 2) {
+            x = TankFrame.GAME_WIDTH - DefaultTank.WIDTH - 2;
+        }
+        if (this.y > TankFrame.GAME_HEIGHT - DefaultTank.HEIGHT - 2) {
+            y = TankFrame.GAME_HEIGHT - DefaultTank.HEIGHT - 2;
+        }
+    }
 }
