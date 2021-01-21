1bafd5545fd286fdf46a5a57cb2422a51a35da47
Jack Liu <947613622@qq.com>
2021-01-21 21:21:28 +0800

    抽象类墙

---
+++ src/main/java/com/lushwe/tank/model/Wall.java
@@ -0,0 +1,52 @@
+package com.lushwe.tank.model;
+
+import com.lushwe.tank.GameObject;
+
+import java.awt.*;
+
+/**
+ * 说明：墙（抽象类）
+ *
+ * @author Jack Liu
+ * @date 2021/1/14 下午9:27
+ * @since 0.1
+ */
+public abstract class Wall extends GameObject {
+
+    /**
+     * 宽
+     */
+    protected int w;
+
+    /**
+     * 高
+     */
+    protected int h;
+
+    /**
+     * 方块
+     */
+    protected Rectangle rect;
+
+    public Wall(int x, int y, int w, int h) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.rect = new Rectangle(x, y, w, h);
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        Color color = g.getColor();
+        g.setColor(getColor());
+        g.fillRect(x, y, w, h);
+        g.setColor(color);
+    }
+
+    protected abstract Color getColor();
+
+    public Rectangle getRect() {
+        return rect;
+    }
+}
