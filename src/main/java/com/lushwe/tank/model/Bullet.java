6986d7cc54ad935f4bb128acd9e793e08121bd32
Jack Liu <947613622@qq.com>
2021-01-26 21:37:22 +0800

    子弹基类完善

--- src/main/java/com/lushwe/tank/model/Bullet.java
+++ src/main/java/com/lushwe/tank/model/Bullet.java
@@ -1,6 +1,8 @@
 package com.lushwe.tank.model;
 
+import com.lushwe.tank.GameModel;
 import com.lushwe.tank.GameObject;
+import com.lushwe.tank.enums.Dir;
 import com.lushwe.tank.enums.Group;
 import com.lushwe.tank.util.ResourceUtils;
 
@@ -18,9 +20,61 @@ public abstract class Bullet extends GameObject {
     public static int WIDTH = ResourceUtils.bulletD.getWidth();
     public static int HEIGHT = ResourceUtils.bulletD.getHeight();
 
-    public abstract Group getGroup();
+    /**
+     * 方向
+     */
+    protected Dir dir;
 
-    public abstract Rectangle getRect();
+    /**
+     * 阵营
+     */
+    protected Group group;
 
-    public abstract void die();
+    /**
+     * 矩形
+     */
+    protected Rectangle rect;
+
+    /**
+     * 活着
+     */
+    protected boolean living = true;
+
+    public Bullet(int x, int y, Dir dir, Group group) {
+        this.x = x;
+        this.y = y;
+        this.dir = dir;
+        this.group = group;
+        this.rect = new Rectangle(x, y, WIDTH, HEIGHT);
+
+        GameModel.getInstance().add(this);
+    }
+
+    public Group getGroup() {
+        return group;
+    }
+
+    public Rectangle getRect() {
+        return rect;
+    }
+
+    public Dir getDir() {
+        return dir;
+    }
+
+    /**
+     * 死亡
+     */
+    public void die() {
+        living = false;
+    }
+
+    /**
+     * 是否活着
+     *
+     * @return
+     */
+    public boolean isLiving() {
+        return living;
+    }
 }
