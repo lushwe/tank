db490703990f2fa426d2b19523d98ed00189fc63
Jack Liu <947613622@qq.com>
2021-02-19 16:07:17 +0800

    游戏工厂接口调整

--- src/main/java/com/lushwe/tank/factory/GameFactory.java
+++ src/main/java/com/lushwe/tank/factory/GameFactory.java
@@ -1,14 +1,13 @@
 package com.lushwe.tank.factory;
 
-import com.lushwe.tank.model.Bullet;
-import com.lushwe.tank.model.Explode;
-import com.lushwe.tank.GameModel;
-import com.lushwe.tank.model.Tank;
 import com.lushwe.tank.enums.Dir;
 import com.lushwe.tank.enums.Group;
+import com.lushwe.tank.model.Bullet;
+import com.lushwe.tank.model.Explode;
+import com.lushwe.tank.model.Tank;
 
 /**
- * 说明：基础游戏工厂
+ * 说明：游戏工厂接口
  *
  * @author Jack Liu
  * @date 2021-1-10 20:27
@@ -23,10 +22,9 @@ public interface GameFactory {
      * @param y
      * @param dir
      * @param group
-     * @param gm
      * @return
      */
-    Tank createTank(int x, int y, Dir dir, Group group, GameModel gm);
+    Tank createTank(int x, int y, Dir dir, Group group);
 
     /**
      * 创建子弹
@@ -35,18 +33,16 @@ public interface GameFactory {
      * @param y
      * @param dir
      * @param group
-     * @param gm
      * @return
      */
-    Bullet createBullet(int x, int y, Dir dir, Group group, GameModel gm);
+    Bullet createBullet(int x, int y, Dir dir, Group group);
 
     /**
      * 创建爆炸
      *
      * @param x
      * @param y
-     * @param gm
      * @return
      */
-    Explode createExplode(int x, int y, GameModel gm);
+    Explode createExplode(int x, int y);
 }
